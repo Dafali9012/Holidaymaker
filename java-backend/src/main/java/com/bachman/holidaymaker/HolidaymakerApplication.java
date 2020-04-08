0285c8f29dce6a6d377130243d7bd5d423aa8023
Daniel <Daniel.falklimberg@gmail.com>
2020-04-08 16:49:44 +0200

    initial commit

---
+++ java-backend/src/main/java/com/bachman/holidaymaker/HolidaymakerApplication.java
@@ -0,0 +1,13 @@
+package com.bachman.holidaymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaymakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaymakerApplication.class, args);
+    }
+
+}
---
+++ java-backend/src/test/java/com/bachman/holidaymaker/HolidaymakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachman.holidaymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaymakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
