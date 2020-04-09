869f8b832c38685248d7190d253f3c098747e438
Daniel <Daniel.falklimberg@gmail.com>
2020-04-09 11:03:23 +0200

    register layout

--- js-frontend/src/views/Register.vue
+++ js-frontend/src/views/Register.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="d-flex flex-column align-items-center register">
-    <div class="col-8 rounded bg-info text-white py-3 pl-5">
-        <p class="text-left">Registrera konto</p>
-        <input type="text" class="form-control col-4 mb-2" placeholder="Email">
-        <input type="text" class="form-control col-4" placeholder="Password">
-        <button type="button" class="btn btn-light">Registrera konto</button>
+    <div class="d-flex justify-content-between col-6 border rounded py-3 pl-5 text-left">
+      <h2>Bachman Hendricks</h2>
+      <div>
+        <router-link to="/register">
+          <button type="button" class="btn btn-light border">Registrera</button>
+        </router-link>
+        <button type="button" class="btn btn-light border rounded-0">Logga in</button>
+      </div>
     </div>
+    <form class="col-6 border rounded py-3 pl-5 text-left">
+      <p class="font-weight-bold">Registrera konto</p>
+      <input type="text" class="form-control col-4 mb-2" placeholder="Email" required />
+      <input type="text" class="form-control col-4 mb-2" placeholder="Password" required />
+      <input type="text" class="form-control col-4 mb-2" placeholder="Namn" required />
+      <input type="text" class="form-control col-4 mb-2" placeholder="Telefon" required />
+      <button type="submit" class="btn btn-light border">Registrera</button>
+    </form>
   </div>
 </template>
