c863eaaa486932962ee5c6a71688a865654c65a7
Daniel <Daniel.falklimberg@gmail.com>
2020-04-13 17:10:23 +0200

    rest stuff

---
+++ java-backend/src/main/java/com/bachman/holidaymaker/rest/UserController.java
@@ -0,0 +1,21 @@
+package com.bachman.holidaymaker.rest;
+
+import com.bachman.holidaymaker.entity.User;
+import com.bachman.holidaymaker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
--- js-frontend/src/views/Register.vue
+++ js-frontend/src/views/Register.vue
@@ -9,13 +9,13 @@
         </router-link>
       </div>
     </div>
-    <form class="col-6 border rounded py-3 pl-5 text-left">
+    <form @submit.prevent="handleSubmit()" class="col-6 border rounded py-3 pl-5 text-left">
       <p class="font-weight-bold">Registrera konto</p>
       <input type="text" class="form-control col-4 mb-2" placeholder="Email" required />
       <input type="text" class="form-control col-4 mb-2" placeholder="Lösenord" required />
       <input type="text" class="form-control col-4 mb-2" placeholder="Namn" required />
       <input type="text" class="form-control col-4 mb-2" placeholder="Telefon" required />
-      <button type="submit" class="btn btn-light border" onsubmit="submitForm()">Registrera</button>
+      <button type="submit" class="btn btn-light border">Registrera</button>
     </form>
   </div>
 </template>
@@ -28,8 +28,8 @@ export default {
     };
   },
   methods: {
-    submitForm() {
-      console.log("form submitted");
+    handleSubmit: function() {
+      console.log("form submitted\nuser created");
     }
   }
 };
