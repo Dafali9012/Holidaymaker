c770f69b72e68599736279830a3203d430281a7b
Anton <anton.ohlin@hotmail.com>
2020-04-14 13:31:21 +0200

    startpage col-fix

--- js-frontend/src/views/Home.vue
+++ js-frontend/src/views/Home.vue
@@ -19,23 +19,24 @@
       <label class="col-3" for="country">Land</label>
       <label class="col-2" for="startdate">Check in</label>
       <label class="col-2" for="enddate">Check out</label>
-      <label class="col-2" for="adults">Antal vuxna</label>
-      <label class="col-2" for="kids">Antal barn</label>
+      <label class="col-1" for="adults">Antal vuxna</label>
+      <label class="col-1.5" for="kids">Antal barn <br> (3-18 år)</label>
+      <label class="col-2" for="kids">Antal småbarn <br> (0-3 år)</label>
       
     </div>
       <div class="row m-2">
-    <select class="border rounded col-3" name="country" id="country">
+    <select class="border rounded col-md-3" name="country" id="country">
     <option value="">-Välj land-</option>
     <option value="Frankrike">Frankrike</option>
     <option value="Spanien">Spanien</option>
     <option value="Luxemburg">Luxemburg</option>
 </select>
 
-      <input type="date" class="border rounded col-2" name="startdate" placeholder="Check in"/>
-            <input type="date" class="border rounded col-2" name="enddate" placeholder="Check out"/>
+      <input type="date" class="border rounded col-md-2" name="startdate" placeholder="Check in"/>
+            <input type="date" class="border rounded col-md-2" name="enddate" placeholder="Check out"/>
 
-    <select class="border rounded col-2" name="adults" id="adults">
-    <option value="">-Välj antal-</option>
+    <select class="border rounded col-md-1" name="adults" id="adults">
+    <option value="0">0</option>
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
@@ -46,8 +47,20 @@
     <option value="8">8</option>
     <option value="9">9</option>
 </select>
-    <select class="border rounded col-2" name="kids" id="kids">
-    <option value="">-Välj antal-</option>
+    <select class="border rounded col-md-1" name="kids" id="kids">
+    <option value="0">0</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+</select>
+    <select class="border rounded col-md-1" name="smallkids" id="smallkids">
+    <option value="0">0</option>
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
