980861132e6aba6f756489d45b4136504c5f1326
Anton <anton.ohlin@hotmail.com>
2020-04-15 14:11:50 +0200

    password covered

--- js-frontend/src/views/Register.vue
+++ js-frontend/src/views/Register.vue
@@ -12,7 +12,7 @@
     <form @submit.prevent="completeRegistration()" class="col-6 border rounded py-3 pl-5 text-left">
       <p class="font-weight-bold">Registrera konto</p>
       <input v-model="email" type="text" class="form-control col-4 mb-2" placeholder="Email" required />
-      <input v-model="password" type="text" class="form-control col-4 mb-2" placeholder="Lösenord" required />
+      <input v-model="password" type="password" class="form-control col-4 mb-2" placeholder="Lösenord" required />
       <input v-model="name" type="text" class="form-control col-4 mb-2" placeholder="Namn" />
       <input v-model="phonenumber" type="text" class="form-control col-4 mb-2" placeholder="Telefon" />
       <button type="submit" class="btn btn-light border">Registrera</button>
