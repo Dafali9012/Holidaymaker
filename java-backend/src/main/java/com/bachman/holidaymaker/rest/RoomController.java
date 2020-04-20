99b208fe005f9f2ceb36ce375fcd93be9c5ea86c
Anton <anton.ohlin@hotmail.com>
2020-04-20 12:07:26 +0200

    room/all, room/id, user/all, user/id

--- java-backend/src/main/java/com/bachman/holidaymaker/rest/RoomController.java
+++ java-backend/src/main/java/com/bachman/holidaymaker/rest/RoomController.java
@@ -1,11 +1,15 @@
 package com.bachman.holidaymaker.rest;
 
 import com.bachman.holidaymaker.entity.Room;
+import com.bachman.holidaymaker.entity.User;
 import com.bachman.holidaymaker.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/room")
@@ -13,8 +17,12 @@ public class RoomController {
     @Autowired
     private RoomRepository roomRepository;
 
-    @GetMapping
+    @GetMapping("all")
     public Iterable<Room> getAllRooms() {
             return roomRepository.findAll();
+    }
+    @GetMapping("{id}")
+    public Iterable<Room> roomById(@PathVariable long id){
+        return this.roomRepository.findAllById(Collections.singleton(id));
     }
 }
