dee60c4f80e2148dd283bfb39329504b779242b6
Anton <anton.ohlin@hotmail.com>
2020-04-20 16:52:55 +0200

    pathing error på home

--- js-frontend/src/store/index.js
+++ js-frontend/src/store/index.js
@@ -16,7 +16,7 @@ export default new Vuex.Store({
   },
   actions: {
     async loadSearchData({commit}) {
-      let response = await fetch("http://localhost:8080/room");
+      let response = await fetch("http://localhost:8080/room/all");
       let data = await response.json();
       commit('changeSearchData', data)
     },
