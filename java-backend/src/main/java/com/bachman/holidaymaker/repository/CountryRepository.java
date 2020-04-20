035d6d52a4c444724265384d7c4872f939b87733
Anton <anton.ohlin@hotmail.com>
2020-04-20 13:27:58 +0200

    country API

--- java-backend/src/main/java/com/bachman/holidaymaker/repository/CountryRepository.java
+++ java-backend/src/main/java/com/bachman/holidaymaker/repository/CountryRepository.java
@@ -1,11 +1,10 @@
 package com.bachman.holidaymaker.repository;
 
-import com.bachman.holidaymaker.entity.Room;
+import com.bachman.holidaymaker.entity.Country;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends CrudRepository<Room, Long> {
+public interface CountryRepository extends CrudRepository<Country, Long> {
 
 }
-
